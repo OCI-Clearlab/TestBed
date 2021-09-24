aa866c85b5425fad1613d05d79cef047fdf34705
cloudical <41122141+cloudical@users.noreply.github.com>
2021-09-24 12:57:33 -0500

    Update build-and-push.sh

--- action-b/build-and-push.sh
+++ action-b/build-and-push.sh
@@ -1,15 +1,12 @@
 #!/usr/bin/env bash
 
 set -eux
-export DOCKER_CLI_EXPERIMENTAL=enabled
 
 VERSION="v1"
-HUB="docker.io/arosilier"
 
 GOOS=linux go build -v -o .
-
-docker-buildx build -t "${HUB}/hello:${VERSION}" .
+docker build -t "arosilier/hello:${VERSION}" .
 
 if [[ -z "${DRY_RUN:-}" ]]; then
-  docker push "${HUB}/hello:${VERSION}"
+  docker push "arosilier/hello:${VERSION}"
 fi
